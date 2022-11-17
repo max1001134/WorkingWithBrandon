05015869e3efbe0bd7e6c35d32b92be85eecff81
WarrenWu4 <warrenweiwu04@gmail.com>
2022-11-17 07:51:34 -0600

    Changed froms stack nav to navbar also added logbox to remove warning

--- App.js
+++ App.js
@@ -3,18 +3,25 @@ import { StyleSheet, Text, View } from 'react-native';
 import { TailwindProvider } from 'tailwindcss-react-native';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ToDoScreen from './screens/To_Do_Screen';
 import LeaderboardScreen from './screens/LeaderboardScreen';
 
+import { LogBox } from 'react-native'; //to suppress stupid key prop warning
+
+
 const Stack = createNativeStackNavigator();
+const Navbar = createBottomTabNavigator();
 
 export default function App() {
+  LogBox.ignoreAllLogs();
   return (
     <NavigationContainer>
       <TailwindProvider>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={LeaderboardScreen} />
-        </Stack.Navigator>
+        <Navbar.Navigator>
+          <Navbar.Screen name="Todo" component={ToDoScreen} />
+          <Navbar.Screen name="Home" component={LeaderboardScreen} />
+        </Navbar.Navigator>
       </TailwindProvider>
     </NavigationContainer>
   );
