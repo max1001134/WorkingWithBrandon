47cbdbaa5cdbfafaf7032079f64ea1e95b1a08ca
WarrenWu4 <warrenweiwu04@gmail.com>
2022-11-17 13:32:44 -0600

    Added and styled navbar

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import LeaderboardScreen from './screens/LeaderboardScreen';
 
 import { LogBox } from 'react-native'; //to suppress stupid key prop warning
 
+//icons
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 const Navbar = createBottomTabNavigator();
@@ -18,9 +20,37 @@ export default function App() {
   return (
     <NavigationContainer>
       <TailwindProvider>
-        <Navbar.Navigator>
-          <Navbar.Screen name="Todo" component={ToDoScreen} />
-          <Navbar.Screen name="Home" component={LeaderboardScreen} />
+        <Navbar.Navigator screenOptions={({route}) => ({
+          tabBarStyle: {
+            position:'absolute',
+            elevation: 0,
+            borderRadius: 15,
+            borderTopWidth: 0,
+            left: 15,
+            right: 15,
+            bottom: 15,
+            height: "7%",
+            backgroundColor: "#ffffff",
+          },
+          tabBarOptions: {showIcon:true},
+          tabBarShowLabel: false,
+        })}>
+          <Navbar.Screen name="Todo" component={ToDoScreen} options={{
+            tabBarIcon: ({focused, size, color}) => (
+              <View style={{alignItems:'center', justifyContent:'center', textAlign:'center'}}>
+                <MaterialIcons name="view-list" size={24} color={focused ? "#FA8072" : "#000000"} />
+                <Text style={{color: focused ? "#FA8072" : "#000000", fontSize: 12}}>To-Do List</Text>
+              </View>
+            ),
+          }}/>
+          <Navbar.Screen name="Home" component={LeaderboardScreen} options={{
+            tabBarIcon: ({focused, size, color}) => (
+              <View style={{alignItems:'center', justifyContent:'center', textAlign:'center'}}>
+                <MaterialIcons name="leaderboard" size={24} color={focused ? "#FA8072" : "#000000"} />
+                <Text style={{color: focused ? "#FA8072" : "#000000", fontSize: 12}}>Leaderboard</Text>
+              </View>
+            ),
+          }}/>
         </Navbar.Navigator>
       </TailwindProvider>
     </NavigationContainer>
